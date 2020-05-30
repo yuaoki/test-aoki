b965cab936df21f419e6cf6733ee21b85156d126
yuaoki <yuaoki22@gmail.com>
2020-05-30 17:35:42 +0900

    ファイルの指定

--- dir.go
+++ dir.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"flag"
 )
 
 func main() {
-	files, err := ioutil.ReadDir(".")
+	var open = flag.String("open", ".", "please specify -name file")
+	flag.Parse()
+	files, err:= ioutil.ReadDir(*open)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(files)
 
-	for _, file := range files {
+	for i, file := range files {
+	    fmt.Print(i)
 		fmt.Println(file.Name())
 	}
 }
